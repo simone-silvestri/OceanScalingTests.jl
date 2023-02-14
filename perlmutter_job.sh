8bb0cf69ae20f9a39821e68357c574d479ab5dbd
Simone Silvestri <ssilvest@login28.chn>
2023-02-13 16:15:02 -0800

    this should work

--- perlmutter_job.sh
+++ perlmutter_job.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+#SBATCH -C gpu
+#SBATCH -q regular
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=1
-#SBATCH --ntasks-per-core=1
-#SBATCH --threads-per-core=1
-#SBATCH --mem=200GB
-#SBATCH --time 06:00:00
+#SBATCH --time=06:00:00
 #SBATCH --account=m1759
+#SBATCH --gpus-per-node=4
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH --gpu-bind=none
 
-module load cuda
 module load cray-mpich
 module load openmpi/3.1.6-cuda-pmi-ucx-slurm-jhklron
 
 export SBATCH_ACCOUNT=m1759
 export SALLOC_ACCOUNT=m1759
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK:=1}
+
+export SLURM_CPU_BIND="cores"
+export CRAY_ACCEL_TARGET="nvidia80"
 
 cat > launch.sh << EoF_s
 #! /bin/sh
@@ -23,5 +27,5 @@ EoF_s
 chmod +x launch.sh
 
 cp perlmutter/* .
-srun --mpi=pmi2 --gpus ./launch.sh julia --check-bounds=no --project= experiments/run.jl ${RESOLUTION:=3}
+srun ./launch.sh julia --check-bounds=no --project experiments/run.jl ${RESOLUTION:=3}
 rm *.toml
