f80483d33934dea7c7f996bee0f6ca91edfa0f2c
Valentin Churavy <v.churavy@gmail.com>
2023-03-31 23:50:38 -0400

    Use a common depot + nsys

--- perlmutter_job.sh
+++ perlmutter_job.sh
@@ -13,9 +13,12 @@ module load cray-mpich
 
 export SBATCH_ACCOUNT=m4367
 export SALLOC_ACCOUNT=m4367
-export PATH=${HOME}/julia-1.8.5/bin:${PATH}
+export COMMON=/global/common/software/m4367
+
+export PATH=${COMMON}/julia-1.9.0-rc2/bin:${PATH}
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK:=1}
 export JULIA_LOAD_PATH="${JULIA_LOAD_PATH}:$(pwd)/perlmutter"
+export JULIA_DEPOT_PATH=":${COMMON}/depot"
 
 export SLURM_CPU_BIND="cores"
 export CRAY_ACCEL_TARGET="nvidia80"
@@ -27,4 +30,5 @@ exec \$*
 EoF_s
 chmod +x launch.sh
 
-srun ./launch.sh julia --check-bounds=no --project experiments/run.jl ${RESOLUTION:=3}
+nsys profile --trace=cuda,mpi,nvtx --mpi-impl=mpich \
+    srun ./launch.sh julia --check-bounds=no --project experiments/run.jl ${RESOLUTION:=3}
