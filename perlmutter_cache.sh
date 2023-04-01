aa50733ff482b8e43d52d0df534c72add88209f0
Valentin Churavy <v.churavy@gmail.com>
2023-03-31 23:56:30 -0400

    fixup! add cache script

--- perlmutter_cache.sh
+++ perlmutter_cache.sh
@@ -5,7 +5,6 @@ export COMMON=/global/common/software/m4367
 export PATH=${COMMON}/julia-1.9.0-rc2/bin:${PATH}
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK:=1}
 export JULIA_LOAD_PATH="${JULIA_LOAD_PATH}:$(pwd)/perlmutter"
-export JULIA_DEPOT_PATH=":${COMMON}/depot"
 
 export SLURM_CPU_BIND="cores"
 export CRAY_ACCEL_TARGET="nvidia80"
@@ -14,4 +13,7 @@ export RESOLUTION=1
 export NZ=12
 export EXPERIMENT=DoubleDrake
 
+JULIA_DEPOT_PATH="${COMMON}/depot" julia --check-bounds=no --project -e "import Pkg; Pkg.instantiate(); Pkg.precompile()"
+
+export JULIA_DEPOT_PATH=":${COMMON}/depot"
 julia --check-bounds=no --project experiments/run.jl
