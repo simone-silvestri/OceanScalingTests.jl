106058dc3e093b32ae4a51fbc4fa551cab4fbfdf
Valentin Churavy <v.churavy@gmail.com>
2023-04-01 16:27:02 -0400

    fixup! add env

--- perlmutter_env.sh
+++ perlmutter_env.sh
@@ -5,3 +5,4 @@ export COMMON=/global/common/software/m4367
 export PATH=${COMMON}/julia-1.9.0-rc2/bin:${PATH}
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK:=1}
 export JULIA_LOAD_PATH="${JULIA_LOAD_PATH}:$(pwd)/perlmutter"
+export JULIA_DEPOT_PATH=":${COMMON}/depot"
