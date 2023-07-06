54f3846a9e5a2b9734a053939fd8ed900f2cb11c
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-07-06 15:00:42 +0200

    correct file link

--- data/download_datasets.jl
+++ data/download_datasets.jl
@@ -1,7 +1,7 @@
 using DataDeps
 
 # Bathymetry data from ETOPO1 converted to .jld2
-path = "https://dl.dropboxusercontent.com/s/33v6tye7sftt1ud/bathymetry-ice-21600x10800.jld2?dl=0"
+path = "https://dl.dropboxusercontent.com/s/4m56hv6hzef5j0j/evolved-initial-conditions-365days.jld2?dl=0"
 
 dh = DataDep("initial_conditions", "1/12th degree resolution initial conditions", path)
 
